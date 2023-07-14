844e02e7a600ca2bb3ec8b1a6921281b11ddee57
Jacky <1606290354@qq.com>
2023-07-14 17:31:52 +0800

    master 4 push

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
+        System.out.println("master 2");
+        System.out.println("hot-fix 3");
         System.out.println("master 3");
+        System.out.println("master 4 push");
     }
 }
